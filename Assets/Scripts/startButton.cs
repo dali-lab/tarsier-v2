8850930f846e75d9887c49fc85b055233a74b05a
Cathy Wu <xinai.cathy.wu@gmail.com>
2020-02-06 14:47:02 -0500

    init lobby w/ start button

---
+++ Assets/Scripts/startButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startButton : MonoBehaviour
+{
+    public Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    void Activate()
+    {
+
+        anim.SetBool("Press", true);
+
+    }
+
+    void Deactivate()
+    {
+
+        anim.SetBool("Press", false);
+
+    }
+}
---
+++ Assets/Scripts/startButtonTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startButtonTrigger : MonoBehaviour
+{
+    // an array allows the same trigger to cause multiple actions
+    // "SerializeField" forces Unity to expose the private variable in editor
+    [SerializeField] private GameObject[] targets;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach (GameObject target in targets)
+        {
+            target.SendMessage("Activate");
+            Debug.Log(target.name);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        foreach (GameObject target in targets)
+        {
+            target.SendMessage("Deactivate");
+        }
+    }
+
+}
