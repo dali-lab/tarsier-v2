402365999a74cba64a459d38f4985c1d0e64fa99
DALI Lab <dali@dali.dartmouth.edu>
2020-02-16 15:31:24 -0500

    Working on additive scene loading in headset, reorganized assets

--- Assets/Scripts/sceneLoader.cs
+++ Assets/Scripts/sceneLoader.cs
@@ -1,37 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEditor.SceneManagement;
+#endif
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 [ExecuteInEditMode]
 public class sceneLoader : MonoBehaviour
 {
-    public SceneAsset[] scenes;
+    public Object[] scenes;
+    public string scenesFolder;
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-    
-        for (int i = 0; i < scenes.Length; i++)
+
+        if (Application.isEditor)
         {
-            if (Application.isEditor)
+#if UNITY_EDITOR
+            for (int i = 0; i < scenes.Length; i++)
             {
                 if (EditorApplication.isPlayingOrWillChangePlaymode)
                 {
                     LoadSceneParameters parameters = new LoadSceneParameters();
                     parameters.loadSceneMode = LoadSceneMode.Additive;
                     SceneManager.UnloadSceneAsync(scenes[i].name);
-                    EditorSceneManager.LoadSceneInPlayMode("Assets/Scenes/" + scenes[i].name + ".unity", parameters);
+                    EditorSceneManager.LoadSceneInPlayMode("Assets/Scenes/" + scenesFolder + "/" + scenes[i].name + ".unity", parameters);
                 }
                 else
                 {
-                    EditorSceneManager.OpenScene("Assets/Scenes/" + scenes[i].name + ".unity", OpenSceneMode.Additive);
+                    EditorSceneManager.OpenScene("Assets/Scenes/" + scenesFolder + "/" + scenes[i].name + ".unity", OpenSceneMode.Additive);
                 }
+            }
+#endif
+        }
+        else
+        {
+            for (int j = 0; j < scenes.Length; j++)
+            {
+                SceneManager.LoadSceneAsync(scenes[j].name, LoadSceneMode.Additive);
             }
 
         }
-
+      
     }
 }
