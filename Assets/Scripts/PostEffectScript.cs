532fba50e5cdf859abfb14a74ef80f3e1e3d62d5
Jasmine Mai <Jasmine.W.Mai.20@dartmouth.edu>
2020-02-15 19:01:14 -0500

    Built branch off of forestBugs branch

---
+++ Assets/Scripts/PostEffectScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PostEffectScript : MonoBehaviour
+{
+    public Material mat;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, mat);
+    }
+}
+
