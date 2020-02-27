cfd9b22fb51db0d439c6d552eb2ddb63eb2c1049
Cathy Wu <xinai.cathy.wu@gmail.com>
2020-02-27 17:28:29 -0500

    initial tutorial

---
+++ Assets/Scripts/rightControls.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class rightControls : MonoBehaviour
+{
+
+    private VRTK_ControllerEvents controllerEvents;
+    public Material[] materials;
+
+    private void OnEnable()
+    {
+        controllerEvents = GetComponent<VRTK_ControllerEvents>();
+        if (controllerEvents == null)
+        {
+            VRTK_Logger.Error(VRTK_Logger.GetCommonMessage(VRTK_Logger.CommonMessageKeys.REQUIRED_COMPONENT_MISSING_FROM_GAMEOBJECT, "VRTK_ControllerEvents_ListenerExample", "VRTK_ControllerEvents", "the same"));
+            return;
+        }
+
+        controllerEvents.ButtonOnePressed += DoButtonOnePressed;
+    }
+
+    private void OnDisable()
+    {
+        if (controllerEvents != null)
+        {
+            controllerEvents.ButtonOnePressed -= DoButtonOnePressed;
+        }
+    }
+
+    private void DebugLogger(uint index, string button, string action, ControllerInteractionEventArgs e)
+    {
+        string debugString = "Controller on index '" + index + "' " + button + " has been " + action
+                             + " with a pressure of " + e.buttonPressure + " / Primary Touchpad axis at: " + e.touchpadAxis + " (" + e.touchpadAngle + " degrees)" + " / Secondary Touchpad axis at: " + e.touchpadTwoAxis + " (" + e.touchpadTwoAngle + " degrees)";
+        VRTK_Logger.Info(debugString);
+    }
+
+    private void DoButtonOnePressed(object sender, ControllerInteractionEventArgs e)
+    {
+        for (int i = 0; i < materials.Length; i++)
+        {
+            if (materials[i].GetFloat("_ColorblindOn") == 1.0f)
+            {
+                materials[i].SetFloat("_ColorblindOn", 0.0f);
+            }
+            else
+            {
+                materials[i].SetFloat("_ColorblindOn", 1.0f);
+            }
+        }
+
+    }
+}
+
+
---
+++ Assets/Scripts/tutorial.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class tutorial : MonoBehaviour
+{
+    public GameObject RController;
+    public GameObject LController;
+    public GameObject[] glowingButtons;
+    public GameObject[] textScreens;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        glowingButtons[0].SetActive(false);
+        glowingButtons[1].SetActive(false);
+        glowingButtons[2].SetActive(false);
+        glowingButtons[3].SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (textScreens[0].activeSelf)                  // on welcome screen
+        {
+            //[wait 5 seconds]
+
+            // welcome message off, press buttons message on
+            textScreens[0].SetActive(false);
+            textScreens[1].SetActive(true);
+
+            // turn on button glow
+            glowingButtons[0].SetActive(true);
+            glowingButtons[1].SetActive(true);
+            glowingButtons[2].SetActive(true);
+            glowingButtons[3].SetActive(true);
+        }
+        else if (textScreens[1].activeSelf)                  // on button screen
+        {                  
+            var RcontrollerEvents = RController.GetComponent<VRTK_ControllerEvents>();
+            var LcontrollerEvents = LController.GetComponent<VRTK_ControllerEvents>();
+
+            if (RcontrollerEvents.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.ButtonOnePress))
+            {
+                glowingButtons[0].SetActive(false); // turn off glow on R button 1
+            }
+            if (RcontrollerEvents.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.ButtonTwoPress))
+            {
+                glowingButtons[1].SetActive(false);     // turn off glow on R button 2
+            }
+            if (LcontrollerEvents.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.ButtonOnePress))
+            {
+                glowingButtons[2].SetActive(false);     // turn off glow on L button 1
+            }
+            if (LcontrollerEvents.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.ButtonTwoPress))
+            {
+                glowingButtons[3].SetActive(false);     // turn off glow on L button 2
+            }
+        }
+
+    }
+}
