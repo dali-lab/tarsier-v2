afde0be704aafead5cd1452f73997f4d967c342f
giga-byt <bill.y.tang.20@dartmouth.edu>
2020-03-01 11:03:00 -0500

    scene warping implemented

---
+++ Assets/Scripts/warpSphereScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class warpSphereScript : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+      Debug.Log("collided");
+      Debug.Log(other.tag);
+      Debug.Log(other.name);
+      if(true){
+          SceneManager.LoadScene("ForestScene", LoadSceneMode.Single);
+          Scene sceneToLoad = SceneManager.GetSceneByName("ForestScene");
+          SceneManager.MoveGameObjectToScene(other.gameObject, sceneToLoad);
+      }
+    }
+}
