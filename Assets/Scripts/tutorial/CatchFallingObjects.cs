40799e04a2962db08e8ede90bbc25e2074fe42b8
DALI Lab <dali@dali.dartmouth.edu>
2020-03-05 09:57:02 -0500

    Added falling objects script

---
+++ Assets/Scripts/tutorial/CatchFallingObjects.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchFallingObjects : MonoBehaviour
+{
+
+    private Vector3 originalPosition;
+    private int originalY;
+    private int originalZ;
+
+    public float minY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.y <= minY)
+        {
+            gameObject.transform.position = originalPosition;
+        }
+    }
+}
