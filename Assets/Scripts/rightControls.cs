8e0098eedda573a435c180858bfbfab2dc227a95
DALI Lab <dali@dali.dartmouth.edu>
2020-03-06 14:21:53 -0500

    Updated scripts

--- Assets/Scripts/rightControls.cs
+++ Assets/Scripts/rightControls.cs
@@ -10,7 +10,11 @@ public class rightControls : MonoBehaviour
     public AudioClip tarsierKatydidAudio;
     //public AudioSource audioSource;
 
+    public Light directionalLight;
+
     private VRTK_ControllerEvents controllerEvents;
+
+    private bool tarsierVision = false;
 
     private void OnEnable()
     {
@@ -41,6 +45,7 @@ public class rightControls : MonoBehaviour
 
     private void DoButtonOnePressed(object sender, ControllerInteractionEventArgs e)
     {
+        tarsierVision = !tarsierVision;
         // material changes
         for (int i = 0; i < materials.Length; i++)
         {
@@ -51,7 +56,16 @@ public class rightControls : MonoBehaviour
             else
             {
                 materials[i].SetFloat("_ColorblindOn", 1.0f);
+                directionalLight.intensity = 0.5f;
             }
+        }
+
+        if (tarsierVision)
+        {
+            directionalLight.intensity = 0.5f;
+        } else
+        {
+            directionalLight.intensity = 0.02f;
         }
 
         // sound changes
