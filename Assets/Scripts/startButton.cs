b47dbe2fa44eca3a93aa0e8e77546f11066c648a
Cathy Wu <xinai.cathy.wu@gmail.com>
2020-04-12 15:33:21 -0400

    Add lobby light animations, update assets

--- Assets/Scripts/startButton.cs
+++ Assets/Scripts/startButton.cs
@@ -5,58 +5,65 @@ using VRTK;
 
 public class startButton : MonoBehaviour
 {
-    // an array allows the same trigger to cause multiple actions
-    // "SerializeField" forces Unity to expose the private variable in editor
-    private Light spotlightObject;
     public GameObject leftController;
     public GameObject rightController;
-    public Animator anim;
-    public bool pressing;
+    public GameObject button;
+    public Animator startButtonAnim;
+    public Animator tarsierSpotlightAnim;
+    public Animator ambientLightAnim;
+    public Material skybox;
+    private float _exposure = 0;
+    private bool _startPressed = false;
 
     void Start()
     {
-      spotlightObject = GameObject.Find("Tarsier Spotlight").GetComponent(typeof(Light)) as Light;
+        RenderSettings.skybox.SetFloat("_Exposure", _exposure);
     }
 
+    IEnumerator turnOnLights()
+    {
+        yield return new WaitForSeconds(0.5f);
+        tarsierSpotlightAnim.SetBool("on", true);
+        yield return new WaitForSeconds(3);
+        ambientLightAnim.SetBool("on", true);
+        yield return new WaitForSeconds(0.5f);
+        while (_exposure < 1)                       // increase brightness of skybox
+        {
+            yield return new WaitForSeconds(0.0005f);
+            brightenSkybox();
+        }
+
+    }
+    private void brightenSkybox()
+    {
+            if (_exposure < 1)
+            {
+                _exposure += 0.005f;
+                RenderSettings.skybox.SetFloat("_Exposure", _exposure);
+            }
+        
+    }
     void Update()
     {
-      Transform leftT = leftController.GetComponent(typeof(Transform)) as Transform;
-      Transform rightT = rightController.GetComponent(typeof(Transform)) as Transform;
-      Transform buttonT = gameObject.GetComponent(typeof(Transform)) as Transform;
+        Transform leftT = leftController.GetComponent(typeof(Transform)) as Transform;
+        Transform rightT = rightController.GetComponent(typeof(Transform)) as Transform;
+        Transform buttonT = button.GetComponent(typeof(Transform)) as Transform;
 
-      VRTK_ControllerEvents leftControllerEvents = leftController.GetComponent<VRTK_ControllerEvents>();
+        VRTK_ControllerEvents leftControllerEvents = leftController.GetComponent<VRTK_ControllerEvents>();
 
-      
-      if(Vector3.Distance(buttonT.position, leftT.position) <= 1 && leftControllerEvents.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.ButtonOnePress)) 
-      {
-        if(pressing == false) {
-          spotlightObject.enabled = !spotlightObject.enabled;
-          anim.SetBool("Press", true);
-          pressing = true;
-        } else {
-          anim.SetBool("Press", true);
+        if (Vector3.Distance(rightT.position, buttonT.position) <= .1 || Vector3.Distance(leftT.position, buttonT.position) <= .1)
+        {
+            if (_startPressed == false)
+            {
+                _startPressed = true;
+                StartCoroutine(turnOnLights());
+            }
+            startButtonAnim.SetBool("Press", true);
         }
-      }
-      if(!leftControllerEvents.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.ButtonOnePress)) {
-        pressing = false;
-        anim.SetBool("Press", false);
-      }
+        else
+        {
+            startButtonAnim.SetBool("Press", false);
+        }
     }
-
-    // private void OnTriggerEnter(Collider other)
-    // {
-    //   if(!pressing)
-    //   {
-    //     spotlightObject.enabled = !spotlightObject.enabled;
-    //   }
-    //   pressing = true;
-    //   anim.SetBool("Press", true);
-    // }
-
-    // private void OnTriggerExit(Collider other)
-    // {
-    //   anim.SetBool("Press", false);
-    //   pressing = false;
-    // }
-
 }
+
