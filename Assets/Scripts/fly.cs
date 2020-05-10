8c885d944e12505566f956b0e1ea00ae2e4f6b0b
Cathy Wu <xinai.cathy.wu@gmail.com>
2020-05-09 20:00:21 -0400

    UV arc and head tilt fly

--- Assets/Scripts/fly.cs
+++ Assets/Scripts/fly.cs
@@ -6,7 +6,9 @@ using VRTK;
 public class fly : MonoBehaviour
 {
     public GameObject centerEye;
+    public GameObject flyParticles;
     public GameObject LController;
+    public GameObject blackScreen;
     private VRTK_ControllerEvents LControllerEvents;
 
 
@@ -18,14 +20,21 @@ public class fly : MonoBehaviour
     {
         Debug.Log("starting");
         LControllerEvents = LController.GetComponent<VRTK_ControllerEvents>();
+        flyParticles.SetActive(false);
+        blackScreen.SetActive(false);
     }
  
     private void Update()
     {
-        if (LControllerEvents.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.TriggerPress))
+        if (LControllerEvents.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.ButtonOnePress))
         {
             isFlying = !isFlying;
             Debug.Log("flying: " + isFlying);
+            flyParticles.SetActive(!flyParticles.activeSelf);
+            if (isFlying == false)
+            {
+                StartCoroutine(fadeToBlack());
+            }
         }
         Fly();
     }
@@ -40,5 +49,11 @@ public class fly : MonoBehaviour
             //transform.position += centerEye.transform.position Rotate(0, increment, 0);
             //Debug.Log(transform.position);
         }
+    }
+    IEnumerator fadeToBlack()
+    {
+        blackScreen.SetActive(true);
+        yield return new WaitForSeconds(2);
+        blackScreen.SetActive(false);
     }
 }
