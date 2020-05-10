625404c2397002bc60cfde97da8369cf8e0736a4
Cathy Wu <xinai.cathy.wu@gmail.com>
2020-05-10 14:54:14 -0400

    with texture stuff

---
+++ Assets/Scripts/material_swapping/MaterialEventManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+public class MaterialEventManager : MonoBehaviour 
+{
+    public delegate void MaterialSwapAction(bool uVmode);
+    public static event MaterialSwapAction OnMaterialSwap;
+
+    public GameObject leftControllerAlias;
+
+    public static bool isUV = false;
+    private static bool locked = false;
+
+    void Start()
+    {
+        leftControllerAlias.GetComponent<VRTK_ControllerEvents>().ButtonTwoPressed += new ControllerInteractionEventHandler(DoButtonTwoPress);
+        leftControllerAlias.GetComponent<VRTK_ControllerEvents>().ButtonTwoReleased += new ControllerInteractionEventHandler(DoButtonTwoRelease);
+    }
+
+    void DoButtonTwoPress(object sender, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("we got a press here");
+        SendMaterialSwapEvent();
+        locked = true;
+    }
+
+    void DoButtonTwoRelease(object sender, ControllerInteractionEventArgs e)
+    {
+        locked = false;
+    }
+    public static void SendMaterialSwapEvent()
+    {
+        if(locked) {
+            return;
+        }
+        isUV = !isUV;
+        if(OnMaterialSwap != null)
+            OnMaterialSwap(isUV);
+    }
+
+    public static void LockPress() {
+        locked = true;
+    }
+
+    public static void UnlockPress() {
+        locked = false;
+    }
+}
+
---
+++ Assets/Scripts/material_swapping/MaterialSwapScript.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class MaterialSwapScript : MonoBehaviour 
+{
+    public Material[] nUVmats;
+    public Material[] UVmats;
+
+    void OnEnable()
+    {
+        MaterialEventManager.OnMaterialSwap += SwapMaterial;
+    }
+
+
+    void OnDisable()
+    {
+        MaterialEventManager.OnMaterialSwap -= SwapMaterial;
+    }
+
+    int GetIndexOfMaterial(Material mat)
+    {
+        for(int i = 0; i < nUVmats.Length; i++) {
+            if((nUVmats[i].name + " (Instance)").Equals(mat.name)) {
+                return i;
+            }
+            if((UVmats[i].name + " (Instance)").Equals(mat.name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    void SwapMaterial(bool uvMode)
+    {
+        SwapMaterialRecursive(gameObject.transform, uvMode);
+    }
+
+    void SwapMaterialRecursive(Transform t, bool uvMode)
+    {
+        GameObject currGameObject = t.gameObject;
+        // if current gameobject has a renderer
+        if(currGameObject.GetComponent<Renderer>() != null){
+            // switch to the correct texture, if valid and switchable
+            int index = GetIndexOfMaterial(currGameObject.GetComponent<Renderer>().material);
+            if(index > -1) {
+                if(uvMode) {
+                    currGameObject.GetComponent<Renderer>().material = UVmats[index];
+                } else {
+                    currGameObject.GetComponent<Renderer>().material = nUVmats[index];
+                }
+            }
+        }
+        // recurse over children
+        foreach( Transform child in t) {
+            SwapMaterialRecursive(child, uvMode);
+        }
+    }
+}
