edeb5dd6329de6abbd613ca0658c926f4d63f910
Arkendorf <amon@ferri.us>
2020-05-31 14:38:19 -0400

    Remade the flower placer script to generate flowers in clusters of the same type

--- Assets/Scripts/GenerateFlowers.cs
+++ Assets/Scripts/GenerateFlowers.cs
@@ -1,15 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class GenerateFlowers : MonoBehaviour
 {
+    [Header("General Settings")]
     [Tooltip("List containing the prefabs for all the flower variants")]
     public List<GameObject> flowers;
-    [Tooltip("The amount of flowers to generate")]
-    public int amount;
     [Tooltip("Half the width and length of the area to generate flowers in (centered on this game object's position")]
     public float range;
+    [Header("Cluster Settings")]
+    [Tooltip("How many clusters to generate")]
+    public int clusters;
+    [Tooltip("The minimum amount of flowers to generate in a cluster")]
+    public int minAmount;
+    [Tooltip("The maximum amount of flowers to generate in a cluster")]
+    public int maxAmount;
+    [Tooltip("The range for each individual cluster")]
+    public float clusterRange;
+    [Header("Flower Settings")]
+    [Tooltip("Minimum scale for the flower")]
+    public float minScale;
+    [Tooltip("Maximum scale for the flower")]
+    public float maxScale;
 
     private GameObject container; // Container game object that is the parent to all the generated flowers
 
@@ -20,27 +32,60 @@ public class GenerateFlowers : MonoBehaviour
         container = new GameObject();
         container.name = "Container";
 
-        // Iterate until the desired amount of flowers have been placed
-        for (int i = 0; i < amount; i++)
+        // Iterate until the desired number of clusters have been generated
+        for (int i = 0; i < clusters; i++)
         {
-            // Instantiate a new flower, randomly choosing one of the flower variants
-            GameObject flower = Instantiate(flowers[Random.Range(0, flowers.Count)]);
-            flower.transform.parent = container.transform; // Set the container to be the new flower's parent
+            // Create the new cluster
+            GameObject cluster = new GameObject();
+            cluster.name = "Cluster" + i.ToString();
+            cluster.transform.parent = container.transform; // Make the container the parent of the cluser
 
-            // Randomly scale and rotate the flower
-            flower.transform.localScale = Vector3.one * Random.Range(.5f, 2f);
-            flower.transform.rotation = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
+            // Get a random position on the terrain to center the cluster on
+            cluster.transform.position = GetValidPosition(gameObject.transform.position, range);
 
-            // Raycast downward from the flower. If it doesn't hit the ground, move it to a new position and try again
-            RaycastHit hit;
-            while (!Physics.Raycast(flower.transform.position, Vector3.down, out hit, Mathf.Infinity))
-            {
-                // Try a different random position
-                flower.transform.position = new Vector3(Random.Range(-range, range), gameObject.transform.position.y, Random.Range(-range, range));
-            }
+            // Choose a type of flower for the cluster
+            GameObject flowerType = flowers[Random.Range(0, flowers.Count)];
 
-            // Once the raycast has hit the ground, set the flower's position to match the point where the raycast hit the ground
-            flower.transform.position = hit.point;
+            // Iterate until a number between the minimum and maximum number of flowers have been placed in the cluster
+            for (int j = 0; j < Random.Range(minAmount, maxAmount); j++)
+            { 
+                // Instantiate a new flower
+                GameObject flower = Instantiate(flowerType);
+                flower.transform.parent = cluster.transform; // Set the cluster to be the new flower's parent
+
+                // Randomly scale and rotate the flower
+                flower.transform.localScale = Vector3.one * Random.Range(minScale, maxScale);
+                flower.transform.rotation = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
+
+                // Put the flower in a random position within the cluster's range
+                flower.transform.position = GetValidPosition(cluster.transform.position, clusterRange);
+            }         
         }
+    }
+
+    // Get a random valid position
+    private Vector3 GetValidPosition(Vector3 position, float range)
+    {
+        // Choose an initial random position
+        Vector3 randomPosition = GetRandomPosition(position, range);
+
+        // Raycast downward. If it doesn't hit the ground, move it to a new position and try again
+        RaycastHit hit;
+        while (!Physics.Raycast(randomPosition, Vector3.down, out hit, Mathf.Infinity))
+        {
+            // Try a different random position
+            randomPosition = GetRandomPosition(position, range);
+        }
+        
+        // Set the random position to the raycast's hit point
+        randomPosition = hit.point;
+
+        return randomPosition; // Return the position
+    }
+
+    // Get a random position
+    private Vector3 GetRandomPosition(Vector3 position, float range)
+    {
+        return new Vector3(position.x + Random.Range(-range, range), 100f, position.z + Random.Range(-range, range));
     }
 }
