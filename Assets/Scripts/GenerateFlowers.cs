1d3f7038873b7936e1afd3e9a75c1da8a272e8a0
Arkendorf <amon@ferri.us>
2020-05-31 12:40:10 -0400

    Optimized and commented the script

--- Assets/Scripts/GenerateFlowers.cs
+++ Assets/Scripts/GenerateFlowers.cs
@@ -4,36 +4,42 @@ using UnityEngine;
 
 public class GenerateFlowers : MonoBehaviour
 {
+    [Tooltip("List containing the prefabs for all the flower variants")]
     public List<GameObject> flowers;
+    [Tooltip("The amount of flowers to generate")]
     public int amount;
+    [Tooltip("Half the width and length of the area to generate flowers in (centered on this game object's position")]
     public float range;
 
-    private GameObject container;
+    private GameObject container; // Container game object that is the parent to all the generated flowers
 
     // Start is called before the first frame update
     void Start()
     {
+        // Create the container
         container = new GameObject();
         container.name = "Container";
 
-        
+        // Iterate until the desired amount of flowers have been placed
         for (int i = 0; i < amount; i++)
         {
+            // Instantiate a new flower, randomly choosing one of the flower variants
             GameObject flower = Instantiate(flowers[Random.Range(0, flowers.Count)]);
-            flower.transform.parent = container.transform;
-            flower.transform.position = new Vector3(Random.Range(-range, range), gameObject.transform.position.y, Random.Range(-range, range));
+            flower.transform.parent = container.transform; // Set the container to be the new flower's parent
+
+            // Randomly scale the flower
             flower.transform.localScale = Vector3.one * Random.Range(.5f, 2f);
 
+            // Raycast downward from the flower. If it doesn't hit the ground, move it to a new position and try again
             RaycastHit hit;
-            if (Physics.Raycast(flower.transform.position, Vector3.down, out hit, Mathf.Infinity))
+            while (!Physics.Raycast(flower.transform.position, Vector3.down, out hit, Mathf.Infinity))
             {
-                flower.transform.position = hit.point;
+                // Try a different random position
+                flower.transform.position = new Vector3(Random.Range(-range, range), gameObject.transform.position.y, Random.Range(-range, range));
             }
-            else
-            {
-                Destroy(flower);
-                i--;
-            }
+
+            // Once the raycast has hit the ground, set the flower's position to match the point where the raycast hit the ground
+            flower.transform.position = hit.point;
         }
     }
 }
