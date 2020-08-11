dcfbc9de4067d96480e8b88e574e458c13cd40d5
Jasmine Mai <Jasmine.W.Mai.20@dartmouth.edu>
2020-08-11 16:20:32 -0700

    remove unnecessary comments

--- Assets/Scripts/Bee/NectarGlob.cs
+++ Assets/Scripts/Bee/NectarGlob.cs
@@ -1,16 +1,13 @@
 using UnityEngine;
-using System.Collections;
-using System;
 using Anivision.PlayerInteraction;
-using JetBrains.Annotations;
 
 public class NectarGlob : MonoBehaviour 
 {
     public NectarShaderIntensityController controller;
 
     private Grabber[] Grabbers;
-    [CanBeNull] private Grabber RGrabber;
-    [CanBeNull] private Grabber LGrabber;
+    private Grabber RGrabber;
+    private Grabber LGrabber;
     void Start()
     {
         if (controller != null) {
@@ -36,17 +33,6 @@ public class NectarGlob : MonoBehaviour
         {
             onUse();
         }
-        
-        // if (Vector3.Distance(transform.position, Grabber.transform.position) <= 0.05)
-        // {
-        //     onUse();
-        // }
-        // if (nectarBar != null){
-        //     if (Vector3.Distance(transform.position, Grabber.transform.position) <= 0.05)
-        //     {
-        //         onUse();
-        //     }
-        // }
     }
 
     void onUse()
@@ -55,8 +41,7 @@ public class NectarGlob : MonoBehaviour
             controller.globConsumed();
             gameObject.GetComponent<Grabbable>().EndGrab();
             gameObject.SetActive(false);
-            // Destroy(gameObject);
-            // nectarBar.GetComponent<NectarUI>().AddHealth(0.1f);
+           
         }
     }
 }
