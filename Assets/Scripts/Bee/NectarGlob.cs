d11687e1e24cc9abf249b255d0d255e2a4e6bfc6
Jasmine Mai <Jasmine.W.Mai.20@dartmouth.edu>
2020-08-11 12:59:19 -0700

    Animal manager system added to bee scene

--- Assets/Scripts/Bee/NectarGlob.cs
+++ Assets/Scripts/Bee/NectarGlob.cs
@@ -1,27 +1,35 @@
 using UnityEngine;
 using System.Collections;
 using System;
+using Anivision.PlayerInteraction;
 
 public class NectarGlob : MonoBehaviour 
 {
     public GameObject nectarBar;                                                                        // nectar health bar on left hand
     public NectarShaderIntensityController controller;
 
+    public Grabber Grabber;
     void Start()
     {
         if(controller != null) {
             controller.incrementGlob();
         }
+
+        // Grabber = FindObjectOfType<Grabber>();
     }
 
     void Update()
     {
-        if (nectarBar != null){
-            if (Vector3.Distance(transform.position, nectarBar.transform.position) <= 0.05)
-            {
-                onUse();
-            }
-        }
+        // if (Vector3.Distance(transform.position, Grabber.transform.position) <= 0.05)
+        // {
+        //     onUse();
+        // }
+        // if (nectarBar != null){
+        //     if (Vector3.Distance(transform.position, Grabber.transform.position) <= 0.05)
+        //     {
+        //         onUse();
+        //     }
+        // }
     }
 
     void onUse()
@@ -29,7 +37,7 @@ public class NectarGlob : MonoBehaviour
         if(controller != null) {
             controller.globConsumed();
             Destroy(gameObject);
-            nectarBar.GetComponent<NectarUI>().AddHealth(0.1f);
+            // nectarBar.GetComponent<NectarUI>().AddHealth(0.1f);
         }
     }
 }
